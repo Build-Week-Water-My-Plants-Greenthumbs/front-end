4b518489420c182c9fc788cc42a8755f19327762
nicole pizzano <pizzano.nicole@gmail.com>
2021-08-18 13:11:21 -0400

    basic forms

--- water-my-plants/src/App.js
+++ water-my-plants/src/App.js
@@ -1,15 +1,36 @@
-
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import SignIn from './components/SignIn';
+import SignUp from './components/SignUp';
+import AddPlantForm from './components/AddPlantForm.js'
+import PlantList from './components/PlantList'
+import Plant from './components/Plant'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-       
-        <h1> Water these plantz! </h1>
-        
-      </header>
-    </div>
+      <Router>
+      <div>
+          <section className="App-body">
+              <Switch>
+                  <Route exact path='/' component={SignIn} />
+                  <Route path='/sign-up' component={SignUp}/>
+                  <Route 
+                      exact path='/add-plant' 
+                      component={AddPlantForm} >
+                  </Route>
+                  <Route 
+                      exact path='/plant-list' 
+                      component={PlantList}>
+                  </Route>
+                  {/* to view an individual plant based on its id*/}
+                  <Route path='/plant/:id' 
+                  component={Plant}/>
+                 
+              </Switch>
+          </section>
+      </div>
+      </Router>
   );
 }
 
