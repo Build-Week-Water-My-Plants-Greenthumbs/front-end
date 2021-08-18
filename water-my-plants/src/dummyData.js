0c1b29d0e3718f02ccb92e007ef088fda0fe6a0e
Sarah D Herren <herren.sarah@icloud.com>
2021-08-18 09:38:27 -0500

    created dummy plantData, and added image assets

---
+++ water-my-plants/src/dummyData.js
@@ -0,0 +1,53 @@
+
+
+export const plantData = [
+    {
+    id: 1,
+    nickname: 'Spike',
+    type: {
+        species: 'Cactus',
+        image: '/images/cactus.jpg'
+    }
+    h20Frequency: 7,
+    
+    },
+    {
+        id: 2,
+        nickname: 'Squish',
+        type: {
+            species: 'Succulent',
+            image: '/images/succulent.jpg'
+        }
+        h20Frequency: 14,
+        
+        },
+        {
+            id: 3,
+            nickname: 'Delicate No Flower',
+            type: {
+                species: 'Ficus',
+                image: '/images/ficus.jpg'
+            }
+            h20Frequency: 10,
+            
+            },
+            {
+                id: 4,
+                nickname: 'Mr. Big',
+                type: {
+                    species: 'Tropical',
+                    image: '/images/tropical.jpg'
+                }
+                h20Frequency: 7,
+            },
+            {
+                id: 5,
+                nickname: 'Clingy',
+                type: {
+                    species: 'Vine',
+                    image: '/images/vine.jpg'
+                }
+                h20Frequency: 3,
+                
+                },
+];
